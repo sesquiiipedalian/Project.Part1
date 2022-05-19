27325f6d410bc988a6f1cd424f80bd0f264ffa12
sesquiiipedalian <87616807+sesquiiipedalian@users.noreply.github.com>
2022-05-19 23:56:20 +0300

    Add files via upload

---
+++ CartView.swift
@@ -0,0 +1,20 @@
+//
+//  CartView.swift
+//  Auth without storyboard
+//
+//  Created by Азамат Кемал on 13.05.2022.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text("Корзина")
+    }
+}
+
+struct CartView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartView()
+    }
+}
